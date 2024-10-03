d542975a2176d90d039034140528bd14b68f8022
Rushil Jadia <rushiljadia@gmail.com>
2024-10-03 16:39:25 -0400

    initial commit

---
+++ src/main/java/com/bgeconsortium/bge/BgeApplication.java
@@ -0,0 +1,13 @@
+package com.bgeconsortium.bge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgeconsortium/bge/BgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgeconsortium.bge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
