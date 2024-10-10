7ae83529784fa31dd1e51804091a54323d607ec9
kaphelps33 <alexphelps2001@gmail.com>
2024-10-10 16:16:12 -0400

    Added basic homepage skeleton

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <h1>BGE</h1>
+            </header>
+            <main>
+              <h2>Medication Simplified</h2>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage
